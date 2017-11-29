c48397d052345c2720915fdd95248d36d08475ad
Henrik Haugberg <henrik.haugberg@gmail.com>
2017-11-29 15:40:32 +0100

    Path fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-export { default as Dropdown } from 'src/forms/Dropdown';
-export { default as Field } from 'src/forms/Field';
-export { default as Form } from 'src/forms/Form';
-export { default as Input } from 'src/forms/Input';
-// export { default as NumberInput } from 'src/forms/NumberInput';
-export { default as Option } from 'src/forms/Option';
-export { default as SelectInput } from 'src/forms/SelectInput';
-export { default as StringInput } from 'src/forms/StringInput';
-export { default as Textarea } from 'src/forms/Textarea';
+export { default as Dropdown } from './forms/Dropdown';
+export { default as Field } from './forms/Field';
+export { default as Form } from './forms/Form';
+export { default as Input } from './forms/Input';
+// export { default as NumberInput } from './forms/NumberInput';
+export { default as Option } from './forms/Option';
+export { default as SelectInput } from './forms/SelectInput';
+export { default as StringInput } from './forms/StringInput';
+export { default as Textarea } from './forms/Textarea';
 
