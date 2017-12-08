28301ecf96b46490017dcbdd1ad6b23ef3fb3663
Henrik Haugberg <henrik.haugberg@gmail.com>
2017-12-08 10:48:15 +0100

    Remove debug code

--- src/forms/ComponentToggle.jsx
+++ src/forms/ComponentToggle.jsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import bemCn from 'bem-cn';
-// import Modal from '../layout/Modal';
 import DropdownToggle from './DropdownToggle';
 import ModalToggle from './ModalToggle';
 
-// const block = bemCn('modal-toggle');
-
 export default class ComponentToggle extends React.PureComponent {
 
-    constructor(props) {
-        super(props);
-
-        console.log('(COMPONENT TOGGLE) - constructor');
-    }
-
     render() {
-        const {
-            className,
-            open,
-            toggle,
-            children,
-            barebones
-        } = this.props;
-
-        console.info('WINDOW WIDTH', window.innerWidth);
-
         if (window.innerWidth >= 800) {
             return (
                 <DropdownToggle { ...this.props } />
@@ -35,33 +14,5 @@ export default class ComponentToggle extends React.PureComponent {
                 <ModalToggle { ...this.props } />
             );
         }
-
-        // return (
-        //     <div className={ block.mix(className)({ open })() }>
-        //         <a className={ block.mix('toggle')() } onClick={ this.toggle }>
-        //             { toggle || '\u00a0' }
-        //         </a>
-
-        //         { children &&
-        //             <Modal open={ open } onClose={ this.toggle } barebones={ barebones }>
-        //                 { children }
-        //             </Modal>
-        //         }
-        //     </div>
-        // );
     }
 }
-
-/*ComponentToggle.propTypes = {
-    className: PropTypes.string,
-    open: PropTypes.bool,
-    toggle: PropTypes.node,
-    onToggle: PropTypes.func,
-    barebones: PropTypes.bool,
-    children: PropTypes.node
-};
-
-ComponentToggle.defaultProps = {
-    open: false,
-    barebones: false
-};*/
